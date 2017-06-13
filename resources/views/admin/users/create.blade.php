4494f03f7533a16ce607e6c27752e408361b8c67
mkhsojib <mkhsojib@gmail.com>
2017-06-13 22:05:25 +0600

    Laravel collective html package added

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -4,4 +4,29 @@
 @section('content')
 
 <h1>Create Users</h1>
+
+    {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store']) !!}
+
+        <div class="form-group">
+
+            {!! Form::label('title', 'Title:') !!}
+            {!! Form::text('title', null, ['class'=>'form-control', 'placeholder'=>'title here']) !!}
+        </div>
+
+        <div class="form-group">
+
+            {!! Form::label('body', 'Body:') !!}
+            {!! Form::text('content', null, ['class'=>'form-control', 'placeholder'=>'body here']) !!}
+
+        </div>
+
+        <div class="form-group">
+
+           {!! Form::submit('create post', ['class'=>'btn btn-primary']) !!}
+
+        </div>
+
+
+    {!! Form::close() !!}
+
 @stop
