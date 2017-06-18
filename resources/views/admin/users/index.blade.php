6474bbbd07cd4f03d6ab436b4246a764306230e9
mkhsojib <mkhsojib@gmail.com>
2017-06-19 05:58:44 +0600

    edit option added

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -26,8 +26,8 @@
             @foreach($users as $user)
                 <tr>
                     <td>{{$user->id}}</td>
-                    <td><img height="50" src="{{$user->photo ? $user->photo->file : 'no user photo'}}"/> </td>
-                    <td>{{$user->name}}</td>
+                    <td><img height="50" src="{{$user->photo ? $user->photo->file : 'http://via.placeholder.com/400x400'}}"/> </td>
+                    <td><a href="{{ route('users.edit', $user->id) }}">{{$user->name}}</a></td>
                     <td>{{$user->email}}</td>
                     <td>{{$user->role->name}}</td>
                     <td>{{$user->is_active == 1 ? 'active' : 'inactive'}}</td>
