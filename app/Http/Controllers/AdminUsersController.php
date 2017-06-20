3f6c738b91cab0e2f1ae56997f3b527df82e3e18
mkhsojib <mkhsojib@gmail.com>
2017-06-20 10:58:34 +0600

    update module added

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersEditRequest;
 use App\Http\Requests\UsersRequest;
 use App\Photo;
 use App\Role;
@@ -46,7 +47,17 @@ class AdminUsersController extends Controller
     {
 
 
-        $input = $request->all();
+        if (trim($request->password) == '') {
+
+            $input = $request->except('password');
+        } else {
+
+            $input = $request->all();
+
+            $input['password'] = bcrypt($request->password);
+
+        }
+
 
         if ($file = $request->file('photo_id')) {
 
@@ -60,8 +71,6 @@ class AdminUsersController extends Controller
             $input['photo_id'] = $photo->id;
         }
 
-
-        $input['password'] = bcrypt($request->password);
 
         User::create($input);
 
@@ -107,9 +116,47 @@ class AdminUsersController extends Controller
      * @param  int $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
-        //
+        $user = User::findOrFail($id);
+
+
+        if (trim($request->password) == '') {
+
+            $input = $request->except('password');
+        } else {
+
+            $input = $request->all();
+
+            $input['password'] = bcrypt($request->password);
+
+        }
+
+
+//        if (!empty($request->get('password'))) {
+//            $input = $request->all();
+//        } else {
+//            $input = $request->except(['password']);
+//        }
+
+
+//        $input = $request->all();
+
+        if ($file = $request->file('photo_id')) {
+
+            $name = time() . $file->getClientOriginalName();
+
+            $file->move('images', $name);
+
+            $photo = Photo::create(['file' => $name]);
+
+            $input['photo_id'] = $photo->id;
+
+        }
+
+        $user->update($input);
+
+        return redirect('/admin/users');
     }
 
     /**
--- resources/views/admin/users/edit.blade.php
+++ resources/views/admin/users/edit.blade.php
@@ -5,67 +5,77 @@
 
     <h1>Edit User</h1>
 
+    <div class="row">
+        @include('includes.form_error')
 
-    <div class="col-sm-4">
+    </div>
+    <div class="row">
 
-        <img src="{{$user->photo ? $user->photo->file : 'http://via.placeholder.com/400x400' }}" alt="" class="img-responsive img-rounded">
+        <div class="col-sm-4">
+
+            <img src="{{$user->photo ? $user->photo->file : 'http://via.placeholder.com/400x400' }}" alt=""
+                 class="img-responsive img-rounded">
+
+        </div>
+
+        <div class="col-sm-9">
+
+            {!! Form::model($user, ['method'=>'PATCH', 'action'=>['AdminUsersController@update',  $user->id], 'files'=>true]) !!}
+
+
+            <div class="form-group">
+
+                {!! Form::label('name', 'Name:') !!}
+                {!! Form::text('name', null, ['class'=>'form-control', 'placeholder'=>'name']) !!}
+            </div>
+
+            <div class="form-group">
+
+                {!! Form::label('email', 'Email:') !!}
+                {!! Form::email('email', null, ['class'=>'form-control', 'placeholder'=>'email']) !!}
+            </div>
+
+            <div class="form-group">
+
+                {!! Form::label('role_id', 'Role:') !!}
+                {!! Form::select('role_id', $roles, null, ['class'=>'form-control', 'placeholder'=>'role']) !!}
+            </div>
+
+            <div class="form-group">
+
+                {!! Form::label('is_active', 'Status:') !!}
+                {!! Form::select('is_active', array(1=>'active', 0=>'inactive'), null, ['class'=>'form-control', 'placeholder'=>'status']) !!}
+            </div>
+
+
+            <div class="form-group">
+
+                {!! Form::label('photo_id', 'File:') !!}
+                {!! Form::file('photo_id', null, ['class'=>'form-control', 'placeholder'=>'title here']) !!}
+            </div>
+
+            <div class="form-group">
+
+                {!! Form::label('password', 'Password:') !!}
+                {!! Form::password('password',  ['class'=>'form-control', 'placeholder'=>'password here']) !!}
+            </div>
+
+            <div class="form-group">
+
+                {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
+
+            </div>
+
+
+            {!! Form::close() !!}
+
+        </div>
 
     </div>
 
-    <div class="col-sm-9">
-
-        {!! Form::model($user, ['method'=>'PATCH', 'action'=>['AdminUsersController@update',  $user->id], 'files'=>true]) !!}
 
 
-        <div class="form-group">
 
-            {!! Form::label('name', 'Name:') !!}
-            {!! Form::text('name', null, ['class'=>'form-control', 'placeholder'=>'name']) !!}
-        </div>
-
-        <div class="form-group">
-
-            {!! Form::label('email', 'Email:') !!}
-            {!! Form::email('email', null, ['class'=>'form-control', 'placeholder'=>'email']) !!}
-        </div>
-
-        <div class="form-group">
-
-            {!! Form::label('role_id', 'Role:') !!}
-            {!! Form::select('role_id', $roles, null, ['class'=>'form-control', 'placeholder'=>'role']) !!}
-        </div>
-
-        <div class="form-group">
-
-            {!! Form::label('is_active', 'Status:') !!}
-            {!! Form::select('is_active', array(1=>'active', 0=>'inactive'), null, ['class'=>'form-control', 'placeholder'=>'status']) !!}
-        </div>
-
-
-        <div class="form-group">
-
-            {!! Form::label('photo_id', 'File:') !!}
-            {!! Form::file('photo_id', null, ['class'=>'form-control', 'placeholder'=>'title here']) !!}
-        </div>
-
-        <div class="form-group">
-
-            {!! Form::label('password', 'Password:') !!}
-            {!! Form::password('password',  ['class'=>'form-control', 'placeholder'=>'password here']) !!}
-        </div>
-
-        <div class="form-group">
-
-            {!! Form::submit('Create User', ['class'=>'btn btn-primary']) !!}
-
-        </div>
-
-
-        {!! Form::close() !!}
-
-    </div>
-
-    @include('includes.form_error')
 
 
 
