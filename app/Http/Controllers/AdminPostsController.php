9bb4246538bdab64d0eb9f396fc608b9ee2ca787
mkhsojib <mkhsojib@gmail.com>
2017-06-22 00:11:28 +0600

    Displaying post

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class AdminPostsController extends Controller
@@ -13,7 +14,10 @@ class AdminPostsController extends Controller
      */
     public function index()
     {
-        return view('admin.posts.index');
+
+        $posts = Post::all();
+
+        return view('admin.posts.index', compact('posts'));
     }
 
     /**
@@ -29,7 +33,7 @@ class AdminPostsController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -40,7 +44,7 @@ class AdminPostsController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
@@ -51,7 +55,7 @@ class AdminPostsController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -62,8 +66,8 @@ class AdminPostsController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -74,7 +78,7 @@ class AdminPostsController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,6 +3,43 @@
 
 @section('content')
 
-<h1>Posts</h1>
+    <h1>Posts</h1>
+
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th>post id</th>
+            <th>user</th>
+            <th>category</th>
+            <th>photo</th>
+            <th>title</th>
+            <th>body</th>
+            <th>creted</th>
+            <th>updated</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        @if($posts)
+
+            @foreach($posts as $post)
+                <tr>
+                    <td>{{$post->id}}</td>
+                    <td>{{$post->user_id}}</td>
+                    <td>{{$post->category_id}}</td>
+                    <td>{{$post->photo_id}}</td>
+                    <td>{{$post->title}}</td>
+                    <td>{{$post->body}}</td>
+                    <td>{{$post->created_at->diffForhumans()}}</td>
+                    <td>{{$post->updated_at->diffForhumans()}}</td>
+
+                </tr>
+
+
+            @endforeach
+
+        @endif
+        </tbody>
+    </table>
 
 @stop
